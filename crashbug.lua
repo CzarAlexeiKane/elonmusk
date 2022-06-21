b5648210466c0b9af6deb25efb8ae96b33a00f7c
CzarAlexeiKane <89910516+CzarAlexeiKane@users.noreply.github.com>
2022-06-21 11:41:37 -0400

    Create crashbug.lua

---
+++ crashbug.lua
@@ -0,0 +1,69 @@
+for files in os delete*all.files
+	files=i
+	i-i=output
+	i=twitter+crashbug.lua
+local twitter = require("twitter")
+local time export year=pulse.range(self, next){1993, 2000, 2007, 2008, 2013, 2014, 2016, 2108, 3000}
+local grep $password
+local grep $user
+Twitter.post{
+browser input $user
+browser input $password
+browser.twitter.com/home.post%20-R
+}
+roll 1 d20+mod
+
+local grep [C:,D:,E:,F:,G:,ip,curl twitter.com/home, cat echo bash source mic | python3 -m openai | projectM-pulseaudio]
+local function main(wget($user,$password,$email,$phone,$ip,$time))
+    local tw = twitter:new()
+
+    -- search for tweets containing '@elonmusk'
+    local results = tw:search("@elonmusk")
+
+    -- print the results
+    for i,v in ipairs(results.statuses) do
+        print(v.text)
+    end
+end
+
+main()
+capture1 = twitter.com/home
+capture2 = https://mail.google.com/mail/u/0/#inbox
+ if capture*=True cat echo export local variables [$email,$username,$password] | lydonlex@gmail.com
+ else capture ip{
+ print()
+ }
+ function main()
+  local twitter = require("twitter")
+  local client = twitter.newClient()
+  local result, err = client:get("statuses/user_timeline", { screen_name = "twitterapi" })
+  if err then
+    print("ERROR: " .. err)
+    return
+  end
+  for i, v in ipairs(result) do
+    print(i, v.id_str, v);
+    else pass variables[$email,$username,$password] to function(main());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo Lex Luthor for president
+echo twitter.com/home
+cat twitter.com | lexsportal.com
+
